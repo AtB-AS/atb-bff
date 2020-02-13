a0f411bb2ad6c6b4e04e73f1ced65f41fd72ee68
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-13 12:03:39 +0100

    update deps; use custom http.Agent with enturClient with keep-alive enabled

--- src/service/impl/entur.ts
+++ src/service/impl/entur.ts
@@ -1,7 +1,19 @@
 import createService from '@entur/sdk';
+import fetch from 'node-fetch';
+import { HttpsAgent as Agent } from 'agentkeepalive';
+
+const agent = new Agent({
+  keepAlive: true
+});
 
 const service = createService({
-  clientName: process.env.CLIENT_NAME || 'atb-mittatb'
+  clientName: process.env.CLIENT_NAME || 'atb-mittatb',
+  fetch: (url, init) => {
+    return fetch(url, {
+      agent,
+      ...init
+    });
+  }
 });
 
 export default service;
