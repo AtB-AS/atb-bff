2ea112d8b2fad25e1a3c5a2f54c4ae3fa574c4e1
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-26 13:31:44 +0100

    Merge

--- src/service/impl/entur.ts
+++ src/service/impl/entur.ts
@@ -1,14 +1,64 @@
 import createService from '@entur/sdk';
 import fetch from 'node-fetch';
 import { HttpsAgent as Agent } from 'agentkeepalive';
+import monitoring from '@google-cloud/monitoring';
 
 const agent = new Agent({
   keepAlive: true
 });
 
+const client = new monitoring.MetricServiceClient();
+
 const service = createService({
   clientName: process.env.CLIENT_NAME || 'atb-mittatb',
   fetch: (url, init) => {
+    console.log('fetch');
+    const match = url.match(/\/(\w+)\//);
+    const endpoint = (match && match[1]) || 'NOT-MAPPED';
+
+    // Prepares an individual data point
+    const dataPoint = {
+      interval: {
+        endTime: {
+          seconds: Date.now() / 1000
+        }
+      },
+      value: {
+        int64Value: 1
+      }
+    };
+
+    // Prepares the time series request
+    const request = {
+      name: client.projectPath('atb-mobility-platform'),
+      timeSeries: [
+        {
+          metric: {
+            type: 'custom.googleapis.com/rpc/entur',
+            labels: {
+              endpoint,
+              method: init?.method,
+              headers: init?.headers,
+              payload: init?.payload
+            }
+          },
+          resource: {
+            type: 'global',
+            labels: {
+              project_id: 'atb-mobility-platform'
+            }
+          },
+          points: [dataPoint]
+        }
+      ]
+    };
+
+    // Writes time series data
+    client
+      .createTimeSeries(request)
+      .then(res => console.log('monitoring request succeeded'))
+      .catch(err => console.error('failed to send monitoring request: ', err));
+
     return fetch(url, {
       agent,
       ...init
