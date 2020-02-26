3725ac6c0401809db8bbc49c7a8854c29d3cea2b
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-26 13:21:35 +0100

    Revert "Merge pull request #6 from AtB-AS/rpc-metrics"
    This reverts commit 1fff013e9066a5dd640a5fb6930d0698358f2d0d, reversing
    changes made to e2b0cfea048eaa508e7536c3f2c53910ca82908b.

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,8 @@ if (process.env.NODE_ENV === 'production') {
   require('@google-cloud/trace-agent').start();
 }
 
-import { Logging } from '@google-cloud/logging';
-
 import { createServer, initializePlugins } from './server';
-import enturClient from './service/impl/entur';
+import enturService from './service/impl/entur';
 import geocoderService from './service/impl/geocoder';
 import stopsService from './service/impl/stops';
 import journeyService from './service/impl/journey';
@@ -31,9 +29,6 @@ process.on('unhandledRejection', err => {
   const server = createServer({
     port: process.env['PORT'] || '8080'
   });
-
-  const logger = new Logging();
-  const enturService = enturClient(logger);
 
   await initializePlugins(server);
   server.route({
