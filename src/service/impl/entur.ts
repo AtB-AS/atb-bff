2fad13de2288f5d92ce0dc49fd6380d3da0d0a22
Torbjørn Fjørtoft <torfjor@fjortoft.org>
2020-02-27 10:27:00 +0100

    initial support for OpenCensus stats -> StackDriver custom metrics

--- src/service/impl/entur.ts
+++ src/service/impl/entur.ts
@@ -1,69 +1,106 @@
 import createService from '@entur/sdk';
 import fetch from 'node-fetch';
 import { HttpsAgent as Agent } from 'agentkeepalive';
-import monitoring from '@google-cloud/monitoring';
+
+import {
+  globalStats,
+  MeasureUnit,
+  TagMap,
+  AggregationType
+} from '@opencensus/core';
+import { StackdriverStatsExporter } from '@opencensus/exporter-stackdriver';
+
+const MEASURE_INTERVAL = 60 * 1000;
+const ENDPOINT_RE = /(http.?:\/\/\S+?\/)(\w+)/;
+
+// The latency in milliseconds
+const enturRps = globalStats.createMeasureDouble(
+  'entur/requests',
+  MeasureUnit.UNIT,
+  'Requests upstream to Entur'
+);
+
+const endpointTagKey = { name: 'endpoint' };
+
+const rpsView = globalStats.createView(
+  'rpc/upstream_entur',
+  enturRps,
+  AggregationType.SUM,
+  [endpointTagKey],
+  'Requests upstream'
+);
+globalStats.registerView(rpsView);
+
+const exporter = new StackdriverStatsExporter({
+  projectId: 'atb-mobility-platform'
+});
+globalStats.registerExporter(exporter);
 
 const agent = new Agent({
   keepAlive: true
 });
 
-const client = new monitoring.MetricServiceClient();
+interface Config {}
 
-const service = createService({
-  clientName: process.env.CLIENT_NAME || 'atb-mittatb',
-  fetch: (url, init) => {
-    console.log('fetch');
-    const match = url.match(/\/(\w+)\//);
-    const endpoint = (match && match[1]) || 'NOT-MAPPED';
+type EndpointMeasure = {
+  endpoint: string;
+};
 
-    // Prepares an individual data point
-    const dataPoint = {
-      interval: {
-        endTime: {
-          seconds: Date.now() / 1000
-        }
-      },
-      value: {
-        int64Value: 1
+type EndpointTotal = {
+  [endpint: string]: number;
+};
+
+const service = (config: Config) => {
+  let requests: EndpointMeasure[] = [];
+
+  setInterval(() => {
+    const totals = requests.reduce((total, measure) => {
+      if (total[measure.endpoint] === undefined) {
+        total[measure.endpoint] = 1;
+      } else {
+        total[measure.endpoint] += 1;
       }
-    };
+      return total;
+    }, {} as EndpointTotal);
 
-    // Prepares the time series request
-    const request = {
-      name: client.projectPath('atb-mobility-platform'),
-      timeSeries: [
-        {
-          metric: {
-            type: 'custom.googleapis.com/rpc/entur',
-            labels: {
-              endpoint,
-              method: init?.method,
-              headers: init?.headers,
-              payload: init?.payload
-            }
-          },
-          resource: {
-            type: 'global',
-            labels: {
-              project_id: 'atb-mobility-platform'
-            }
-          },
-          points: [dataPoint]
-        }
-      ]
-    };
+    const metrics = Object.entries(totals).map(([endpoint, reqs]) => ({
+      measure: enturRps,
+      value: reqs,
+      tag: endpoint
+    }));
 
-    // Writes time series data
-    client
-      .createTimeSeries(request)
-      .then(res => console.log('monitoring request succeeded'))
-      .catch(err => console.error('failed to send monitoring request: ', err));
-
-    return fetch(url, {
-      agent,
-      ...init
+    metrics.forEach(m => {
+      const tagmap = new TagMap();
+      tagmap.set(endpointTagKey, { value: m.tag });
+      globalStats.record(
+        [
+          {
+            measure: m.measure,
+            value: m.value
+          }
+        ],
+        tagmap
+      );
     });
-  }
-});
 
+    requests = [];
+  }, MEASURE_INTERVAL);
+
+  return createService({
+    clientName: process.env.CLIENT_NAME || 'atb-mittatb',
+    fetch: (url, init) => {
+      let endpoint = 'UNKNOWN';
+      const match = url.match(ENDPOINT_RE);
+      if (match) {
+        endpoint = match[2];
+      }
+      requests = [...requests, { endpoint }];
+
+      return fetch(url, {
+        agent,
+        ...init
+      });
+    }
+  });
+};
 export default service;
