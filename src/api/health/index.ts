ab32fdf072ea5b6dd360a859793f5e792c76a3fa
Torbjørn Fjørtoft <torbjorn@fjortoft.org>
2020-05-19 10:32:53 +0200

    Update path. Revisit later.

--- src/api/health/index.ts
+++ src/api/health/index.ts
@@ -3,7 +3,7 @@ import Hapi from '@hapi/hapi';
 export default (server: Hapi.Server) => {
   server.route({
     method: 'GET',
-    path: '/v1/health',
+    path: '/bff/health',
     handler: (request, h, err) => {
       return 'OK';
     }
