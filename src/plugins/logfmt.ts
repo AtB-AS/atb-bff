275ecb4cca783fc8c0ec2438e98b774f5ad72aba
Torbjørn Fjørtoft <torbjorn@fjortoft.org>
2020-06-09 12:58:47 +0200

    Defer logging until the response is sent
    This allows us to get the final status code after the handler has
    succeeded or returned an error.
    
    Attach "err" to the log line if there is an error.

--- src/plugins/logfmt.ts
+++ src/plugins/logfmt.ts
@@ -2,19 +2,18 @@ import Hapi, { Server } from '@hapi/hapi';
 import logfmt from 'logfmt';
 import * as stream from 'stream';
 import { Logger } from '../types/logfmt';
+import { Boom } from '@hapi/boom';
 
 interface LogFmtOptions {
   stream?: stream.Writable;
   defaultFields?: (request: Hapi.Request) => Record<string, string>;
 }
 
-const discardLogger = new stream.Writable(
-  {
-    write(chunk, encoding, callback) {
-      setImmediate(callback);
-    }
+const discardLogger = new stream.Writable({
+  write(chunk, encoding, callback) {
+    setImmediate(callback);
   }
-);
+});
 
 const plugin: Hapi.Plugin<LogFmtOptions> = {
   dependencies: 'atb-headers',
@@ -46,9 +45,15 @@ const plugin: Hapi.Plugin<LogFmtOptions> = {
       };
     };
     server.decorate('request', 'logfmt', logger, { apply: true });
-    server.ext('onPreResponse', (request, h) => {
-      request.logfmt.log();
+    server.ext('onPreResponse', (request, h, err) => {
+      if (request.response instanceof Boom) {
+        request.logfmt.with('err', request.response.message);
+      }
       return h.continue;
+    });
+    server.events.on('response', request => {
+      request.logfmt.with('code', request.raw.res.statusCode.toString());
+      request.logfmt.log();
     });
   },
   name: 'logfmt'
