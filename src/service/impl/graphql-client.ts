43a7b1fe360c8678a4cea1fe3d96fb8fb7da668d
Mikael Brevik <mikaelbre@gmail.com>
2020-07-23 08:29:37 +0200

    Sets proper cache policies on default options

--- src/service/impl/graphql-client.ts
+++ src/service/impl/graphql-client.ts
@@ -10,11 +10,11 @@ const link = new HttpLink({
 
 const defaultOptions: DefaultOptions = {
   watchQuery: {
-    fetchPolicy: 'no-cache',
+    fetchPolicy: 'cache-and-network',
     errorPolicy: 'ignore'
   },
   query: {
-    fetchPolicy: 'no-cache',
+    fetchPolicy: 'network-only',
     errorPolicy: 'all'
   }
 };
