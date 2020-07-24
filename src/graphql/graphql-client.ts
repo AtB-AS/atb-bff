2990a42146ad71946cd54324fbae4d7dcd27fde8
Mikael Brevik <mikaelbre@gmail.com>
2020-07-24 09:16:59 +0200

    Reuse node-fetch as client for apollo-link-http

--- src/graphql/graphql-client.ts
+++ src/graphql/graphql-client.ts
@@ -1,11 +1,15 @@
 import ApolloClient, { DefaultOptions } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
-import fetch from 'cross-fetch';
+import fetch from 'node-fetch';
 
 const link = new HttpLink({
   uri: 'https://api.entur.io/journey-planner/v2/graphql',
-  fetch
+
+  // node-fetch uses a different signature than the browser implemented fetch
+  // But we use node-fetch's agent option in other parts of the project.
+  // The functionallity overlaps so this works as expected.
+  fetch: (fetch as unknown) as WindowOrWorkerGlobalScope['fetch']
 });
 
 const defaultOptions: DefaultOptions = {
