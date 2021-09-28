731b6f035dfbb838859c5431a9306e17c0fb6f42
Gøran Dalum <goran.dalum@kantega.no>
2021-09-28 09:41:35 +0200

    feat: Entur staging env config

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,6 @@ export const TICKET_INVITE_KEY: string | undefined =
   process.env.TICKET_INVITE_KEY;
 export const PERIOD_TICKET_INVITE_KEY: string | undefined =
   process.env.PERIOD_TICKET_INVITE_KEY;
+export const ENTUR_ENV: string | undefined = process.env.ENTUR_ENV;
 
 export const ET_CLIENT_NAME = process.env.CLIENT_NAME || 'atb-bff';
--- src/graphql/graphql-client.ts
+++ src/graphql/graphql-client.ts
@@ -2,7 +2,7 @@ import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
 import ApolloClient, { DefaultOptions } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import fetch from 'node-fetch';
-import { ET_CLIENT_NAME } from '../config/env';
+import { ENTUR_ENV, ET_CLIENT_NAME } from '../config/env';
 
 const defaultOptions: DefaultOptions = {
   watchQuery: {
@@ -15,7 +15,10 @@ const defaultOptions: DefaultOptions = {
   }
 };
 
-const urlJourneyPlanner = 'https://api.entur.io/journey-planner/v2/graphql';
+const urlJourneyPlanner =
+  ENTUR_ENV === 'staging'
+    ? 'https://api.staging.entur.io/journey-planner/v2/graphql'
+    : 'https://api.entur.io/journey-planner/v2/graphql';
 
 function createClient(url: string) {
   const cache = new InMemoryCache();
