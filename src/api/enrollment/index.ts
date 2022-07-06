7d0f76270f3484c8259472c0101968f1bd6ac333
Espen MH <espen@monopoint.net>
2022-07-06 12:34:40 +0200

    flex ticketing enroll

--- src/api/enrollment/index.ts
+++ src/api/enrollment/index.ts
@@ -2,7 +2,7 @@ import Hapi from '@hapi/hapi';
 import { EnrollmentQuery } from '../../service/types';
 import { postEnrollmentGroupRequest } from './schema';
 import * as Boom from '@hapi/boom';
-import { PERIOD_TICKET_INVITE_KEY, TICKET_INVITE_KEY } from '../../config/env';
+import { PERIOD_TICKET_INVITE_KEY, TICKET_INVITE_KEY, FLEX_TICKET_INVITE_KEY } from '../../config/env';
 
 export default (server: Hapi.Server) => () => {
   server.route({
@@ -27,6 +27,16 @@ export default (server: Hapi.Server) => () => {
         return {
           status: 'ok',
           groups: ['ticketing_group', 'period_ticketing_group']
+        };
+      }
+
+      if (
+        FLEX_TICKET_INVITE_KEY &&
+        FLEX_TICKET_INVITE_KEY === query.inviteKey
+      ) {
+        return {
+          status: 'ok',
+          groups: ['ticketing_group', 'flex_ticketing_group']
         };
       }
 
