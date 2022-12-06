1dc4895e00ecefd11070dd48badbdcbe7b956682
Tor K Moseng <tor@moseng.no>
2022-12-06 15:28:23 +0100

    remove config option + add host parameter

--- test/src/k6.ts
+++ test/src/k6.ts
@@ -1,18 +1,18 @@
 /*
 - Functional test: 1 user and 1 iteration
-- Performance test: X users over Y seconds (see config/performance.json)
+- Performance test: X users over Y seconds (see config/performanceConfig.json)
     - NB! CURRENTLY NOT IMPLEMENTED
 
 Run (functional tests) - default: 1 user, 1 iteration, usecase 'bff'
 $  yarn webpack
-$  k6 run dist/k6.js --config=dist/config/functional.json --env environment=[staging, prod]
+$  k6 run dist/k6.js --env [environment=[staging, prod] || host=https://...]
 
 With detailed output (see k6.json#handleSummary())
 $  yarn webpack
-$  k6 run dist/k6.js --config=dist/config/functional.json  --env environment=[staging, prod] --out csv=results/output.csv
+$  k6 run dist/k6.js --env [environment=[staging, prod] || host=https://...] --out csv=results/output.csv
 Debugging
 $  yarn webpack
-$  k6 run dist/k6.js --config=dist/config/functional.json --env environment=[staging, prod] [--http-debug || --http-debug="full"]
+$  k6 run dist/k6.js --env [environment=[staging, prod] || host=https://...] [--http-debug || --http-debug="full"]
  */
 
 import { Options } from 'k6/options';
@@ -21,16 +21,16 @@ import { scn } from './scenario';
 import { createJUnitCheckOutput } from './utils/log';
 //WIP
 /* @ts-ignore */
-import {
-  jUnit,
-  textSummary
-} from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 //Settings for the simulation
 export let options: Options = {
-  //vus / duration / iterations set in config file
-  //All custom metrics are defined in 'conf/configuration'
-  //thresholds are set in config file (config/functional.json and possibly config/performance.json)
+  // If more parameters are added to 'conf/*Config.json', add them here as well
+  vus: conf.options().vus,
+  iterations: conf.options()?.iterations,
+  summaryTrendStats: conf.options().summaryTrendStats,
+  thresholds: conf.options().thresholds,
+  stages: conf.options()?.stages
 };
 
 //Before the simulation starts
@@ -49,15 +49,17 @@ export default () => {
   //Run usecase as defined from config - or cli
   scn(conf.usecase());
 
-  //Create JUnit log lines for a post-k6 to pick up
-  createJUnitCheckOutput();
+  //Create JUnit log lines for a post-k6-processor to pick up
+  if (conf.options().junitCheckOutput) {
+    createJUnitCheckOutput();
+  }
 };
 
 //Handle reporting
 export const handleSummary = (data: object) => {
   console.log('Preparing the end-of-test summary...');
-  console.log('-- ' + conf.runFrom());
-  console.log('-- ' + conf.host());
+  console.log('-- runFrom: ' + conf.runFrom());
+  console.log('-- host: ' + conf.host());
 
   //github actions
   if (conf.runFrom() === 'root') {
