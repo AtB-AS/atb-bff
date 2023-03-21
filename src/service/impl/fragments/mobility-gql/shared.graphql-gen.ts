3f03d354cbcce212bf286c83dc7ed909077cd6da
Kristian Selnæs <kristian@selnaes.net>
2023-03-21 14:01:50 +0100

    Extract SystemFragment

--- src/service/impl/fragments/mobility-gql/shared.graphql-gen.ts
+++ src/service/impl/fragments/mobility-gql/shared.graphql-gen.ts
@@ -1,7 +1,9 @@
 import * as Types from '../../../../graphql/mobility/mobility-types_v2';
 
+import { BrandAssetsFragment, RentalAppsFragment } from './vehicles.graphql-gen';
 import { DocumentNode } from 'graphql';
 import gql from 'graphql-tag';
+import { BrandAssetsFragmentDoc, RentalAppsFragmentDoc } from './vehicles.graphql-gen';
 export type OperatorFragment = { id: string, name: TranslatedStringFragment };
 
 export type PricingPlanFragment = { price: number, perKmPricing?: Array<PricingSegmentFragment>, perMinPricing?: Array<PricingSegmentFragment> };
@@ -12,6 +14,27 @@ export type TranslatedStringFragment = { translation: Array<TranslationFragment>
 
 export type TranslationFragment = { language: string, value: string };
 
+export type SystemFragment = { operator: OperatorFragment, name: TranslatedStringFragment, brandAssets?: BrandAssetsFragment, rentalApps?: RentalAppsFragment };
+
+export const PricingSegmentFragmentDoc = gql`
+    fragment pricingSegment on PricingSegment {
+  rate
+  end
+  interval
+  start
+}
+    `;
+export const PricingPlanFragmentDoc = gql`
+    fragment pricingPlan on PricingPlan {
+  perKmPricing {
+    ...pricingSegment
+  }
+  price
+  perMinPricing {
+    ...pricingSegment
+  }
+}
+    ${PricingSegmentFragmentDoc}`;
 export const TranslationFragmentDoc = gql`
     fragment translation on Translation {
   language
@@ -33,25 +56,25 @@ export const OperatorFragmentDoc = gql`
   }
 }
     ${TranslatedStringFragmentDoc}`;
-export const PricingSegmentFragmentDoc = gql`
-    fragment pricingSegment on PricingSegment {
-  rate
-  end
-  interval
-  start
-}
-    `;
-export const PricingPlanFragmentDoc = gql`
-    fragment pricingPlan on PricingPlan {
-  perKmPricing {
-    ...pricingSegment
+export const SystemFragmentDoc = gql`
+    fragment system on System {
+  operator {
+    ...operator
   }
-  price
-  perMinPricing {
-    ...pricingSegment
+  name {
+    ...translatedString
+  }
+  brandAssets {
+    ...brandAssets
+  }
+  rentalApps {
+    ...rentalApps
   }
 }
-    ${PricingSegmentFragmentDoc}`;
+    ${OperatorFragmentDoc}
+${TranslatedStringFragmentDoc}
+${BrandAssetsFragmentDoc}
+${RentalAppsFragmentDoc}`;
 export type Requester<C = {}, E = unknown> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R>
 export function getSdk<C, E>(requester: Requester<C, E>) {
   return {
--- src/service/impl/fragments/mobility-gql/vehicles.graphql-gen.ts
+++ src/service/impl/fragments/mobility-gql/vehicles.graphql-gen.ts
@@ -1,9 +1,9 @@
 import * as Types from '../../../../graphql/mobility/mobility-types_v2';
 
-import { TranslatedStringFragment, PricingPlanFragment, OperatorFragment } from './shared.graphql-gen';
+import { TranslatedStringFragment, PricingPlanFragment, SystemFragment } from './shared.graphql-gen';
 import { DocumentNode } from 'graphql';
 import gql from 'graphql-tag';
-import { TranslatedStringFragmentDoc, PricingPlanFragmentDoc, OperatorFragmentDoc } from './shared.graphql-gen';
+import { TranslatedStringFragmentDoc, PricingPlanFragmentDoc, SystemFragmentDoc } from './shared.graphql-gen';
 export type VehicleTypeFragment = { id: string, formFactor: Types.FormFactor, maxRangeMeters?: number, name?: TranslatedStringFragment };
 
 export type RentalUrisFragment = { android?: string, ios?: string };
@@ -14,7 +14,7 @@ export type RentalAppsFragment = { android?: RentalAppFragment, ios?: RentalAppF
 
 export type BrandAssetsFragment = { brandImageUrl: string, brandImageUrlDark?: string, brandLastModified: string };
 
-export type VehicleFragment = { id: string, lat: number, lon: number, isReserved: boolean, isDisabled: boolean, currentRangeMeters: number, currentFuelPercent?: number, availableUntil?: string, vehicleType: VehicleTypeFragment, pricingPlan: PricingPlanFragment, system: { operator: OperatorFragment, name: TranslatedStringFragment, brandAssets?: BrandAssetsFragment, rentalApps?: RentalAppsFragment }, rentalUris?: RentalUrisFragment };
+export type VehicleFragment = { id: string, lat: number, lon: number, isReserved: boolean, isDisabled: boolean, currentRangeMeters: number, currentFuelPercent?: number, availableUntil?: string, vehicleType: VehicleTypeFragment, pricingPlan: PricingPlanFragment, system: SystemFragment, rentalUris?: RentalUrisFragment };
 
 export const VehicleTypeFragmentDoc = gql`
     fragment vehicleType on VehicleType {
@@ -72,18 +72,7 @@ export const VehicleFragmentDoc = gql`
     ...pricingPlan
   }
   system {
-    operator {
-      ...operator
-    }
-    name {
-      ...translatedString
-    }
-    brandAssets {
-      ...brandAssets
-    }
-    rentalApps {
-      ...rentalApps
-    }
+    ...system
   }
   rentalUris {
     ...rentalUris
@@ -91,10 +80,7 @@ export const VehicleFragmentDoc = gql`
 }
     ${VehicleTypeFragmentDoc}
 ${PricingPlanFragmentDoc}
-${OperatorFragmentDoc}
-${TranslatedStringFragmentDoc}
-${BrandAssetsFragmentDoc}
-${RentalAppsFragmentDoc}
+${SystemFragmentDoc}
 ${RentalUrisFragmentDoc}`;
 export type Requester<C = {}, E = unknown> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R>
 export function getSdk<C, E>(requester: Requester<C, E>) {
