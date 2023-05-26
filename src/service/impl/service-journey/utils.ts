b2df3fd7fb84428430ada9f61521ea6eefc5a287
ingvisim <ingvild.simonsen@atb.no>
2023-05-26 15:53:56 +0200

    refactor

--- src/service/impl/service-journey/utils.ts
+++ src/service/impl/service-journey/utils.ts
@@ -1,6 +1,5 @@
 import polyline from '@mapbox/polyline';
 import haversineDistance from 'haversine-distance';
-import { PointsOnLink } from '../../../graphql/journey/journeyplanner-types_v3';
 import { Coordinates, MapLeg, ServiceJourneyMapInfoData } from '../../types';
 import {
   MapInfoWithFromAndToQuayV2Query,
@@ -23,84 +22,55 @@ export function mapToMapLegs(
     ? [data.toQuay.latitude ?? 0, data.toQuay.longitude ?? 0]
     : undefined;
 
-  const coordinates: PolylinePair[] = polyline.decode(
+  const pointsCoords: PolylinePair[] = polyline.decode(
     data.serviceJourney?.pointsOnLink?.points ?? ''
   );
-  const defaultValue = {
-    start: polypairToCoordinates(coordinates[0]),
-    stop: polypairToCoordinates(coordinates[coordinates.length - 1]),
 
-    mapLegs: [
-      {
-        ...baseItem,
-        faded: false,
-        pointsOnLink: data.serviceJourney?.pointsOnLink as PointsOnLink
-      } as MapLeg
-    ]
-  };
+  const mainStartIndex = fromQuayCoordinates
+    ? findIndex(pointsCoords, fromQuayCoordinates)
+    : 0;
+  const mainEndIndex = toQuayCoordinates
+    ? findIndex(pointsCoords, toQuayCoordinates)
+    : pointsCoords.length - 1;
 
-  if (!fromQuayCoordinates && !toQuayCoordinates) {
-    return defaultValue;
-  }
+  const beforeLegCoords = pointsCoords.slice(0, mainStartIndex + 1);
+  const mainLegCoords = pointsCoords.slice(mainStartIndex, mainEndIndex + 1);
+  const afterLegCoords = pointsCoords.slice(mainEndIndex);
 
-  const splitIndexTo = findIndex(coordinates, toQuayCoordinates);
-  const splitIndexFrom = findIndex(coordinates, fromQuayCoordinates);
+  const toMapLeg = (item: PolylinePair[], faded: boolean): MapLeg =>
+    ({
+      ...baseItem,
+      faded,
+      pointsOnLink: {
+        length: item.length,
+        points: polyline.encode(item)
+      }
+    } as MapLeg);
 
-  if (splitIndexFrom < 0 && splitIndexTo < 0) {
-    return defaultValue;
-  }
-  const isNoneOrFirst = splitIndexFrom < 1;
-  const isNoneOrLast =
-    splitIndexTo < 0 || splitIndexTo >= coordinates.length - 1;
-
-  // before should also include quay.
-  const before = isNoneOrFirst ? [] : coordinates.slice(0, splitIndexFrom + 1);
-  const mainLeg = coordinates.slice(
-    isNoneOrFirst ? 0 : splitIndexFrom,
-    isNoneOrLast ? coordinates.length : splitIndexTo + 1
-  );
-  const after = isNoneOrLast ? [] : coordinates.slice(splitIndexTo);
-
-  const itemIfDefined = (
-    item: PolylinePair[],
-    faded: boolean
-  ): MapLeg | undefined =>
-    item.length
-      ? ({
-          ...baseItem,
-          faded,
-          pointsOnLink: {
-            length: item.length,
-            points: polyline.encode(item)
-          }
-        } as MapLeg)
-      : undefined;
   const mapLegs: MapLeg[] = [
-    itemIfDefined(before, true),
-    itemIfDefined(mainLeg, false),
-    itemIfDefined(after, true)
-  ].filter(Boolean) as MapLeg[];
+    toMapLeg(beforeLegCoords, true),
+    toMapLeg(mainLegCoords, false),
+    toMapLeg(afterLegCoords, true)
+  ].filter(leg => (leg.pointsOnLink.length || 0) > 1);
 
   return {
-    ...defaultValue,
+    start: polypairToCoordinates(pointsCoords[0]),
+    stop: polypairToCoordinates(pointsCoords[pointsCoords.length - 1]),
     mapLegs
   };
 }
 
 function findIndex<T>(
   array: Array<PolylinePair>,
-  quayCoords?: PolylinePair
+  quayCoords: PolylinePair
 ): number {
-  if (!quayCoords) return -1;
-  let closestIndex = -1;
-  let closestDistance = 100;
-  array.forEach((t, index) => {
-    if (haversineDistance(t, quayCoords) < closestDistance) {
-      closestIndex = index;
-      closestDistance = haversineDistance(t, quayCoords);
-    }
-  });
-  return closestIndex;
+  return array.reduce(
+    (closest, t, index) => {
+      const distance = haversineDistance(t, quayCoords);
+      return distance < closest.distance ? { index, distance } : closest;
+    },
+    { index: -1, distance: 100 }
+  ).index;
 }
 
 function polypairToCoordinates(
