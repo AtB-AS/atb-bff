3c7c2baccb758644c0a0a7e6ed0696cb2943964f
kamilatwork <76559623+kamilatwork@users.noreply.github.com>
2023-05-30 15:37:00 +0200

    conditional GraphQL logging & additional rate group
    Skips logging of GraphQL responses completely if no rate limiting
    headers are present.

--- src/graphql/graphql-client.ts
+++ src/graphql/graphql-client.ts
@@ -66,23 +66,36 @@ function createClient(url: string) {
     const loggingLink = new ApolloLink((operation, forward) => {
       return forward(operation).map(response => {
         const context = operation.getContext();
-        const operationNameGroup =
-          operation.operationName == 'Trips' ? 'trips' : 'nontrip';
+        const url = context.response.url;
 
-        const log = {
-          time: new Date(context.response.headers.get('date')).toISOString(),
-          message: 'graphql call',
-          url: context.response.url,
-          code: context.response.status,
-          rateLimitUsed: context.response.headers.get('rate-limit-used'),
-          rateLimitAllowed: context.response.headers.get('rate-limit-allowed'),
-          rateLimitGroup: operationNameGroup,
-          correlationId: headers['correlationId'],
-          requestId: headers['requestId'],
-          installId: headers['installId'],
-          appVersion: headers['appVersion']
-        };
-        console.log(JSON.stringify(log));
+        let operationNameGroup;
+        if (url.includes('/mobility')) {
+          operationNameGroup = 'mobility';
+        } else if (url.includes('/journey-planner')) {
+          operationNameGroup =
+            operation.operationName == 'Trips' ? 'trips' : 'nontrip';
+        }
+
+        const rateLimitUsed = context.response.headers.get('rate-limit-used');
+        const rateLimitAllowed =
+          context.response.headers.get('rate-limit-allowed');
+
+        if (rateLimitUsed && rateLimitAllowed) {
+          const log = {
+            time: new Date(context.response.headers.get('date')).toISOString(),
+            message: 'graphql call',
+            url: url,
+            code: context.response.status,
+            rateLimitUsed: rateLimitUsed,
+            rateLimitAllowed: rateLimitAllowed,
+            rateLimitGroup: operationNameGroup,
+            correlationId: headers['correlationId'],
+            requestId: headers['requestId'],
+            installId: headers['installId'],
+            appVersion: headers['appVersion']
+          };
+          console.log(JSON.stringify(log));
+        }
         return response;
       });
     });
