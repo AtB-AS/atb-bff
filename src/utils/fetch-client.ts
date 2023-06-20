3d94fce2adb9386c4f9bd1c1d6eadbe2751eddc4
Johannes Røsvik <j.rosvik@gmail.com>
2023-06-20 14:01:05 +0200

    refactor

--- src/utils/fetch-client.ts
+++ src/utils/fetch-client.ts
@@ -1,13 +1,14 @@
 import fetch, {RequestInit, Response} from 'node-fetch';
 import {ENTUR_BASEURL, ET_CLIENT_NAME} from '../config/env';
 
-const baseURL = ENTUR_BASEURL || 'https://api.entur.io';
+const enturBaseUrl = ENTUR_BASEURL || 'https://api.entur.io';
 
 const performFetch = async (
   url: string,
   init: RequestInit = {},
+  baseUrl: string = enturBaseUrl,
 ): Promise<Response> => {
-  const response = await fetch(`${baseURL}${url}`, {
+  const response = await fetch(`${baseUrl}${url}`, {
     ...init,
     headers: {
       'ET-Client-Name': ET_CLIENT_NAME,
@@ -20,27 +21,37 @@ const performFetch = async (
 export const get = async <T>(
   url: string,
   options: RequestInit = {},
+  baseUrl?: string,
 ): Promise<T> => {
-  const response = await performFetch(url, {
-    ...options,
-    method: 'GET',
-  });
+  const response = await performFetch(
+    url,
+    {
+      ...options,
+      method: 'GET',
+    },
+    baseUrl,
+  );
   return response.json() as T;
 };
 
 export const post = async <T>(
   url: string,
   body: object,
-  options: RequestInit = {},
+  options: RequestInit,
+  baseUrl?: string,
 ): Promise<T> => {
-  const response = await performFetch(url, {
-    ...options,
-    headers: {
-      'Content-Type': 'application/json',
-      ...options.headers,
+  const response = await performFetch(
+    url,
+    {
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.headers,
+      },
+      method: 'POST',
+      body: JSON.stringify(body),
     },
-    method: 'POST',
-    body: JSON.stringify(body),
-  });
+    baseUrl,
+  );
   return response.json() as T;
 };
