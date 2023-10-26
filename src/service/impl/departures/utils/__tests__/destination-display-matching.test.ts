9ff3316d290cef7807092c0ff1dae4ba6a0f6870
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2023-10-26 16:03:38 +0200

    Add dd tests with - and / with and without spaces

--- src/service/impl/departures/utils/__tests__/destination-display-matching.test.ts
+++ src/service/impl/departures/utils/__tests__/destination-display-matching.test.ts
@@ -73,11 +73,65 @@ describe('match destinationDisplays with via as string array or in frontText', (
     ).toBe(false);
   });
 
-  it('matches the original format from src with the new format from favorite with a dash in the via text', () => {
+  it('matches the original format from src with a dash in the via text with the new format from favorite', () => {
     expect(
       destinationDisplaysAreMatching(
         {frontText: 'Lund via Lade-sentrum-Kolstad', via: []},
         {frontText: 'Lund', via: ['Lade', 'sentrum', 'Kolstad']},
+      ),
+    ).toBe(true);
+  });
+
+  it('matches the original format from src with a dash and dot and space in the via text with the new format from favorite', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Havstad via Lerkendal-St. Olavs H.', via: []},
+        {frontText: 'Havstad', via: ['Lerkendal', 'St. Olavs H.']},
+      ),
+    ).toBe(true);
+  });
+
+  it('does not match the original format from src with a dash and dot and space in the via text with only frontText matching', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Havstad via Lerkendal-St. Olavs H.', via: []},
+        {frontText: 'Havstad', via: []},
+      ),
+    ).toBe(false);
+  });
+
+  it('matches the original format from src with a slash and dot in the frontText with the new format from favorite', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Fortuna/Sæterb. via Strindheim', via: []},
+        {frontText: 'Fortuna/Sæterb.', via: ['Strindheim']},
+      ),
+    ).toBe(true);
+  });
+
+  it('matches the original format from src with a dash and spaces in the via text with the new format from favorite', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Trolla via sentrum - Ila', via: []},
+        {frontText: 'Trolla', via: ['sentrum', 'Ila']},
+      ),
+    ).toBe(true);
+  });
+
+  it('matches the original format from src with a slash in the via text with the new format from favorite', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Tempe via Brundalen/Jakobsli', via: []},
+        {frontText: 'Tempe', via: ['Brundalen', 'Jakobsli']},
+      ),
+    ).toBe(true);
+  });
+
+  it('matches the original format from src with a slash and spaces in the via text with the new format from favorite', () => {
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Spongdal via Klett / Skjetlein', via: []},
+        {frontText: 'Spongdal', via: ['Klett', 'Skjetlein']},
       ),
     ).toBe(true);
   });
