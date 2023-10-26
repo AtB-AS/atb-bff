ad36d57b4b3b67c45c55b384be6402d601577e7f
marius-at-atb <134292729+marius-at-atb@users.noreply.github.com>
2023-10-26 15:42:23 +0200

    Inline destinationDisplay test objects

--- src/service/impl/departures/utils/__tests__/destination-display-matching.test.ts
+++ src/service/impl/departures/utils/__tests__/destination-display-matching.test.ts
@@ -1,68 +1,57 @@
 import {destinationDisplaysAreMatching} from '../favorites';
 
-const ddSrcOrig = {
-  frontText: 'Hallset via sentrum',
-  via: [],
-};
-
-const ddSrcNew = {
-  frontText: 'Hallset',
-  via: ['sentrum'],
-};
-
-const ddFavoriteOrig = {
-  frontText: 'Hallset via sentrum',
-  via: [],
-};
-
-const ddFavoriteNew = {
-  frontText: 'Hallset',
-  via: ['sentrum'],
-};
-
-const ddFavoriteSubstringOther = {
-  frontText: 'Hallset',
-  via: [],
-};
-
-const ddDashedViaOrig = {
-  frontText: 'Lund via Lade-sentrum-Kolstad',
-  via: [],
-};
-
-const ddDashedViaNew = {
-  frontText: 'Lund',
-  via: ['Lade', 'sentrum', 'Kolstad'],
-};
-
 describe('match destinationDisplays with via as string array or in frontText', () => {
   it('matches original formats', () => {
-    expect(destinationDisplaysAreMatching(ddSrcOrig, ddFavoriteOrig)).toBe(
-      true,
-    );
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Hallset via sentrum', via: []},
+        {frontText: 'Hallset via sentrum', via: []},
+      ),
+    ).toBe(true);
   });
 
   it('matches the original format from src with the new format from favorite', () => {
-    expect(destinationDisplaysAreMatching(ddSrcNew, ddFavoriteOrig)).toBe(true);
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Hallset', via: ['sentrum']},
+        {frontText: 'Hallset via sentrum', via: []},
+      ),
+    ).toBe(true);
   });
 
   it('matches the new format from src with the original format from favorite', () => {
-    expect(destinationDisplaysAreMatching(ddSrcOrig, ddFavoriteNew)).toBe(true);
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Hallset via sentrum', via: []},
+        {frontText: 'Hallset', via: ['sentrum']},
+      ),
+    ).toBe(true);
   });
 
   it('matches the new format from src with the new format from favorite', () => {
-    expect(destinationDisplaysAreMatching(ddSrcNew, ddFavoriteNew)).toBe(true);
+    expect(
+      destinationDisplaysAreMatching(
+        {frontText: 'Hallset', via: ['sentrum']},
+        {frontText: 'Hallset', via: ['sentrum']},
+      ),
+    ).toBe(true);
   });
 
   it('does not match another for a frontText substring', () => {
     expect(
-      destinationDisplaysAreMatching(ddSrcNew, ddFavoriteSubstringOther),
+      destinationDisplaysAreMatching(
+        {frontText: 'Hallset', via: ['sentrum']},
+        {frontText: 'Hallset', via: []},
+      ),
     ).toBe(false);
   });
 
   it('matches the original format from src with the new format from favorite with a dash in the via text', () => {
     expect(
-      destinationDisplaysAreMatching(ddDashedViaOrig, ddDashedViaNew),
+      destinationDisplaysAreMatching(
+        {frontText: 'Lund via Lade-sentrum-Kolstad', via: []},
+        {frontText: 'Lund', via: ['Lade', 'sentrum', 'Kolstad']},
+      ),
     ).toBe(true);
   });
 });
