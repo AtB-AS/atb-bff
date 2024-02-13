f36153609fe068d5cd1bd47006ecf8db9f38d2ef
Gøran Dalum <goran.dalum@gmail.com>
2024-02-13 12:21:17 +0100

    feat: Add booking arrangements to departure queries

--- src/service/impl/fragments/journey-gql/estimated-calls.graphql-gen.ts
+++ src/service/impl/fragments/journey-gql/estimated-calls.graphql-gen.ts
@@ -5,7 +5,8 @@ import gql from 'graphql-tag';
 import { QuayFragmentDoc } from './quays.graphql-gen';
 import { NoticeFragmentDoc } from './notices.graphql-gen';
 import { SituationFragmentDoc } from './situations.graphql-gen';
-export type EstimatedCallWithQuayFragment = { actualArrivalTime?: any, actualDepartureTime?: any, aimedArrivalTime: any, aimedDepartureTime: any, cancellation: boolean, date: any, expectedDepartureTime: any, expectedArrivalTime: any, forAlighting: boolean, forBoarding: boolean, realtime: boolean, destinationDisplay?: { frontText?: string, via?: Array<string> }, quay: { id: string, name: string, publicCode?: string, stopPlace?: { id: string, name: string, latitude?: number, longitude?: number }, tariffZones: Array<{ id: string, name?: string }> }, notices: Array<{ id: string, text?: string }>, situations: Array<{ id: string, situationNumber?: string, reportType?: Types.ReportType, summary: Array<{ language?: string, value: string }>, description: Array<{ language?: string, value: string }>, advice: Array<{ language?: string, value: string }>, infoLinks?: Array<{ uri: string, label?: string }>, validityPeriod?: { startTime?: any, endTime?: any } }> };
+import { BookingArrangementFragmentDoc } from './booking-arrangements.graphql-gen';
+export type EstimatedCallWithQuayFragment = { actualArrivalTime?: any, actualDepartureTime?: any, aimedArrivalTime: any, aimedDepartureTime: any, cancellation: boolean, date: any, expectedDepartureTime: any, expectedArrivalTime: any, forAlighting: boolean, forBoarding: boolean, realtime: boolean, destinationDisplay?: { frontText?: string, via?: Array<string> }, quay: { id: string, name: string, publicCode?: string, stopPlace?: { id: string, name: string, latitude?: number, longitude?: number }, tariffZones: Array<{ id: string, name?: string }> }, notices: Array<{ id: string, text?: string }>, situations: Array<{ id: string, situationNumber?: string, reportType?: Types.ReportType, summary: Array<{ language?: string, value: string }>, description: Array<{ language?: string, value: string }>, advice: Array<{ language?: string, value: string }>, infoLinks?: Array<{ uri: string, label?: string }>, validityPeriod?: { startTime?: any, endTime?: any } }>, bookingArrangements?: { bookingMethods?: Array<Types.BookingMethod>, latestBookingTime?: any, bookingNote?: string, bookWhen?: Types.PurchaseWhen, minimumBookingPeriod?: string, bookingContact?: { contactPerson?: string, email?: string, url?: string, phone?: string, furtherDetails?: string } } };
 
 export const EstimatedCallWithQuayFragmentDoc = gql`
     fragment estimatedCallWithQuay on EstimatedCall {
@@ -33,10 +34,14 @@ export const EstimatedCallWithQuayFragmentDoc = gql`
   situations {
     ...situation
   }
+  bookingArrangements {
+    ...bookingArrangement
+  }
 }
     ${QuayFragmentDoc}
 ${NoticeFragmentDoc}
-${SituationFragmentDoc}`;
+${SituationFragmentDoc}
+${BookingArrangementFragmentDoc}`;
 export type Requester<C = {}, E = unknown> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R>
 export function getSdk<C, E>(requester: Requester<C, E>) {
   return {
