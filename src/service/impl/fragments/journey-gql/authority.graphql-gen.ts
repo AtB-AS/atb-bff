a8fe779b0e2684fad4eacaf12902b697811092ac
Johannes Røsvik <j.rosvik@gmail.com>
2024-05-14 11:35:21 +0200

    feat: add authority info to trip

---
+++ src/service/impl/fragments/journey-gql/authority.graphql-gen.ts
@@ -0,0 +1,20 @@
+import * as Types from '../../../../graphql/journey/journeyplanner-types_v3';
+
+import { DocumentNode } from 'graphql';
+import gql from 'graphql-tag';
+export type AuthorityFragment = { id: string, name: string, url?: string };
+
+export const AuthorityFragmentDoc = gql`
+    fragment authority on Authority {
+  id
+  name
+  url
+}
+    `;
+export type Requester<C = {}> = <R, V>(doc: DocumentNode, vars?: V, options?: C) => Promise<R> | AsyncIterable<R>
+export function getSdk<C>(requester: Requester<C>) {
+  return {
+
+  };
+}
+export type Sdk = ReturnType<typeof getSdk>;
