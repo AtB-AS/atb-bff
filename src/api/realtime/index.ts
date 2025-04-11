bac4444885792e5ba1e894f8c0f9500f7d5db063
Johannes Røsvik <j.rosvik@gmail.com>
2025-04-11 11:05:53 +0200

    chore: remove deprecated realtime endpoint

--- src/api/realtime/index.ts
+++ src/api/realtime/index.ts
@@ -6,25 +6,6 @@ import {getDepartureRealtime} from './schema';
 export default (server: Hapi.Server) => (service: IRealtimeService) => {
   server.route({
     method: 'GET',
-    path: '/bff/v1/departures-realtime',
-    options: {
-      tags: ['api', 'stops', 'departures', 'realtime'],
-      validate: getDepartureRealtime,
-      description:
-        'Get updated realtime information of all lines and quays passed as data',
-      plugins: {
-        'hapi-swagger': {
-          deprecated: true,
-        },
-      },
-    },
-    handler: async (request, h) => {
-      const query = request.query as unknown as DepartureRealtimeQuery;
-      return (await service.getDepartureRealtime(query, h.request)).unwrap();
-    },
-  });
-  server.route({
-    method: 'GET',
     path: '/bff/v2/departures/realtime',
     options: {
       tags: ['api', 'quays', 'departures', 'realtime'],
