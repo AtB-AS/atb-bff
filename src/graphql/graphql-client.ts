e537765c0c58ba75430923ac38032004b88c173f
Håkon Strandlie <hakon.strandlie@atb.no>
2025-06-11 11:12:27 +0200

    fix: Added stringification of locations to avoid Location: [object Object] on errors

--- src/graphql/graphql-client.ts
+++ src/graphql/graphql-client.ts
@@ -69,7 +69,7 @@ function createClient(url: string) {
       let error = '';
       if (graphQLErrors) {
         graphQLErrors.forEach(({message, locations, path}) => {
-          error += `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}\n`;
+          error += `[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}\n`;
         });
       }
       if (networkError) {
