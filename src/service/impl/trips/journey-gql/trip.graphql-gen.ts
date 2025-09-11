75059c30097b6152d9a8ad4c5a606d3be067f16a
Christian Brevik <christian.brevik@gmail.com>
2025-09-11 11:25:29 +0200

    fix: Narrow non-transit queries for bike rentals to city bikes (#409)
    * fix: Narrow non-transit queries for bike rentals to city bikes
    
    * fix: Add in .graphql as well, obviously

--- src/service/impl/trips/journey-gql/trip.graphql-gen.ts
+++ src/service/impl/trips/journey-gql/trip.graphql-gen.ts
@@ -78,6 +78,7 @@ export const TripsNonTransitDocument = gql`
     arriveBy: $arriveBy
     walkSpeed: $walkSpeed
     modes: {directMode: bike_rental, transportModes: []}
+    whiteListed: {rentalNetworks: ["trondheimbysykkel", "bergenbysykkel", "oslobysykkel", "kolumbusbysykkel", "fartebysykkel"]}
   ) @include(if: $includeBikeRental) {
     ...trip
   }
