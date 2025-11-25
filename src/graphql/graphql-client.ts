4d5cc5eb39840facffec9d675a647455668522b3
Johannes Røsvik <j.rosvik@gmail.com>
2025-11-25 15:55:45 +0100

    feat: migrate to graphql-ws

--- src/graphql/graphql-client.ts
+++ src/graphql/graphql-client.ts
@@ -6,7 +6,7 @@ import {
   HttpLink,
   ApolloLink,
 } from '@apollo/client/core';
-import {WebSocketLink} from '@apollo/client/link/ws';
+import {GraphQLWsLink} from '@apollo/client/link/subscriptions';
 import {onError} from '@apollo/client/link/error';
 import {
   ENTUR_BASEURL,
@@ -14,7 +14,7 @@ import {
   ET_CLIENT_NAME,
 } from '../config/env';
 import WebSocket from 'ws';
-import {SubscriptionClient} from 'subscriptions-transport-ws';
+import {createClient as createWsClient} from 'graphql-ws';
 import {ReqRefDefaults, Request} from '@hapi/hapi';
 import {logResponse} from '../utils/log-response';
 import {Timer} from '../utils/timer';
@@ -123,16 +123,13 @@ function createWebSocketClient(url: string) {
     addTypename: false,
   });
 
-  const wsLink = new WebSocketLink(
-    new SubscriptionClient(
+  const wsLink = new GraphQLWsLink(
+    createWsClient({
       url,
-      {
-        reconnect: true,
-        lazy: true,
-        minTimeout: 10000,
-      },
-      WebSocket,
-    ),
+      webSocketImpl: WebSocket,
+      retryAttempts: 10,
+      lazy: true,
+    }),
   );
 
   const errorLink = onError((error) =>
