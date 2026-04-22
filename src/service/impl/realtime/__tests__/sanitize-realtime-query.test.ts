596bd9c95b2eed4e66cf6452247262c7e8ffb19d
Johannes Røsvik <j.rosvik@gmail.com>
2026-04-22 13:46:10 +0200

    feat: fetch realtime updates for departures slightly in the past (#445)

--- src/service/impl/realtime/__tests__/sanitize-realtime-query.test.ts
+++ src/service/impl/realtime/__tests__/sanitize-realtime-query.test.ts
@@ -9,37 +9,40 @@ const createQuery = (startTime: Date): DepartureRealtimeQuery => ({
 });
 
 describe('sanitizeRealtimeQuery', () => {
-  it('When start time is now the sanitized start time should be unchanged and time range should be 30 minutes', () => {
+  it('When start time is now the sanitized start time should be 3 minutes in the past and time range should be 30+3 minutes', () => {
     const startTime = new Date();
     const sanitized = sanitizeRealtimeQuery(createQuery(startTime));
 
     expect(sanitized?.timeRange).toBeDefined();
     expect(
-      diff(sanitized!.startTime.getTime(), startTime.getTime()),
+      diff(sanitized!.startTime.getTime(), addMinutes(startTime, -3).getTime()),
     ).toBeLessThan(5);
-    expect(diff(sanitized!.timeRange, 1800)).toBeLessThanOrEqual(1);
+    expect(diff(sanitized!.timeRange, 33 * 60)).toBeLessThanOrEqual(1);
   });
 
-  it('When start time is in the past the sanitized start time should be now and time range should be 30 minutes', () => {
+  it('When start time is in the past the sanitized start time should be 3 minutes in the past and time range should be 30+3 minutes', () => {
     const startTime = addHours(new Date(), -2);
     const sanitized = sanitizeRealtimeQuery(createQuery(startTime));
 
     expect(sanitized?.timeRange).toBeDefined();
     expect(
-      diff(sanitized!.startTime.getTime(), new Date().getTime()),
+      diff(
+        sanitized!.startTime.getTime(),
+        addMinutes(new Date(), -3).getTime(),
+      ),
     ).toBeLessThan(5);
-    expect(diff(sanitized!.timeRange, 1800)).toBeLessThanOrEqual(1);
+    expect(diff(sanitized!.timeRange, 33 * 60)).toBeLessThanOrEqual(1);
   });
 
-  it('When start time is 15 minutes in the future the sanitized start time should be unchanged and time range should be 15 minutes', () => {
+  it('When start time is 15 minutes in the future the sanitized start time should be 15-3 minutes in the future and time range should be 15+3 minutes', () => {
     const startTime = addMinutes(new Date(), 15);
     const sanitized = sanitizeRealtimeQuery(createQuery(startTime));
 
     expect(sanitized?.timeRange).toBeDefined();
     expect(
-      diff(sanitized!.startTime.getTime(), startTime.getTime()),
+      diff(sanitized!.startTime.getTime(), addMinutes(startTime, -3).getTime()),
     ).toBeLessThan(5);
-    expect(diff(sanitized!.timeRange, 900)).toBeLessThanOrEqual(1);
+    expect(diff(sanitized!.timeRange, 18 * 60)).toBeLessThanOrEqual(1);
   });
 
   it('When start time is 45 minutes in the future the sanitized query should be undefined', () => {
