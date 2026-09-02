361a3aada59f4cfb41d349d81729280f486a5121
Gøran Dalum <goran.dalum@mittatb.no>
2026-09-02 07:57:31 +0200

    feat: requestStop support necessary for favorites and departure details (#475)

--- src/service/impl/departures-grouped/utils/grouping.ts
+++ src/service/impl/departures-grouped/utils/grouping.ts
@@ -56,6 +56,7 @@ type DepartureTime = {
   serviceJourneyId?: string;
   serviceDate: string;
   cancellation: boolean;
+  requestStop: boolean;
   bookingArrangements?: BookingArrangementFragment;
   notices?: NoticeFragment[];
   stopPositionInPattern: number;
@@ -168,6 +169,7 @@ export default function mapQueryToGroups(
               serviceJourneyId: time.serviceJourney?.id,
               serviceDate: time.date,
               cancellation: time.cancellation,
+              requestStop: time.requestStop,
               notices: time.notices,
               bookingArrangements: time.bookingArrangements,
               stopPositionInPattern: time.stopPositionInPattern,
